09c3bba02d83de8b9df709e722e50219dac81576
Deepraj Sutar <deeprajsutar@Deeprajs-MacBook-Air.local>
2025-07-09 20:49:26 +0530

    Final fix: Ensure bcryptjs is used everywhere

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 // import bcrypt from "bcrypt";
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
